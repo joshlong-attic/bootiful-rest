0a758d4a7e48d7155befb1185f97115e4d01f9fc
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-11-16 20:16:08 +0100

    renaming for consistency

--- code/auth/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into account(account_name , password) values( 'dsyer', 'password');
-insert into account(account_name , password) values( 'jlong', 'password');
-insert into account(account_name , password) values( 'rwinch', 'password');
--- code/auth/src/main/resources/schema.sql
+++
@@ -1,4 +0,0 @@
-create table account ( ACCOUNT_NAME varchar(255) not null,
-                      PASSWORD VARCHAR (255 ) not null,
-                      ID serial ,
-                      ENABLED bool default true);
