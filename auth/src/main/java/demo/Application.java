234fff9398be5afb942263e39792b70ab868e99e
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-11-16 02:09:20 -0800

    adding auth project for an OAuth2 authorization-server.

---
+++ auth/src/main/java/demo/Application.java
@@ -0,0 +1,86 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(JdbcTemplate jdbcTemplate) {
+        RowMapper<User> userRowMapper = (resultSet, i) -> {
+            boolean enabled = resultSet.getBoolean("ENABLED");
+            return new User(
+                    resultSet.getString("ACCOUNT_NAME"),
+                    resultSet.getString("PASSWORD"),
+                    enabled,
+                    enabled,
+                    enabled,
+                    enabled,
+                    AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN")
+            );
+        };
+        return username -> jdbcTemplate.queryForObject("select * from ACCOUNT where ACCOUNT_NAME = ?", userRowMapper, username);
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            ObjectPostProcessor<Object> objectPostProcessor,
+            UserDetailsService userDetailsService) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        return authenticationManagerBuilder.getOrBuild();
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    static class OAuth2AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("acme")
+                    .secret("acmesecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                    .scopes("read");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer.checkTokenAccess("permitAll()");
+        }
+
+    }
+}
+
---
+++ auth/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into account(account_name , password) values( 'dsyer', 'password');
+insert into account(account_name , password) values( 'jlong', 'password');
+insert into account(account_name , password) values( 'rwinch', 'password');
